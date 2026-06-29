bc1ce2bbe73c99cd1db1604ca3adaf2f4a6382e1
anexainc <anexa.inc@gmail.com>
2026-06-29 01:33:52 -0300

    Adiciona dados do tema e script de importacao automatica

--- core/wsgi.py
+++ core/wsgi.py
@@ -15,3 +15,19 @@ try:
         print("Superusuário criado com sucesso via WSGI!")
 except Exception as e:
     print(f"Erro ao criar superusuário: {e}")
+
+
+# --- SCRIPT PARA IMPORTAR O TEMA AUTOMATICAMENTE ---
+try:
+    from django.core.management import call_command
+    from django.contrib.auth import get_user_model
+    
+    # Caminho do arquivo json que vai subir pro Render
+    tema_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'tema_casajoao.json')
+    
+    if os.path.exists(tema_path):
+        # Executa o comando de importação dentro do servidor do Render
+        call_command('loaddata', tema_path)
+        print("Tema personalizado importado com sucesso via WSGI!")
+except Exception as e:
+    print(f"Erro ao importar o tema: {e}")
