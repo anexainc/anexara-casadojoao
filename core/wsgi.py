05e502fd35bd081eba696c659a2409bfa9a80e36
anexainc <anexa.inc@gmail.com>
2026-06-29 01:14:58 -0300

    Adiciona script de autocriação de superuser no wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+# --- SCRIPT PARA CRIAR O SUPERUSUÁRIO AUTOMATICAMENTE ---
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    # Altere aqui para o usuário e senha que você deseja usar no Render:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('vpictures', 'valmonpictures@gmail.com', 'minhasenha123')
+        print("Superusuário criado com sucesso via WSGI!")
+except Exception as e:
+    print(f"Erro ao criar superusuário: {e}")
